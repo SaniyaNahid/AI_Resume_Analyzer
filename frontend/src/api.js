cd4ccbedf5b0659cf505da526343ba1c4567f1cd
Student <student@example.com>
2026-08-01 21:39:40 +0530

    chore(frontend): update API endpoint for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/analyze";
+const API_URL = "https://ai-resume-analyzer-backend-3d0d.onrender.com/analyze";
 
 export const analyzeResume = async (formData) => {
   const response = await axios.post(API_URL, formData);
